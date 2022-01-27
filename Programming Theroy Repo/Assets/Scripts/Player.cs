299fab1bcce0d2cf26dec64ad8d9a8daf8c7e742
Miguel <30949939+donFaifo@users.noreply.github.com>
2022-01-27 07:17:17 +0100

    Main scene and player behaviour updated. Now it works.

--- Programming Theroy Repo/Assets/Scripts/Player.cs
+++ Programming Theroy Repo/Assets/Scripts/Player.cs
@@ -26,13 +26,14 @@ public class Player : Character
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput, Space.Self);
-        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * horizontalInput, Space.Self);
+        transform.Translate(speed * Time.deltaTime * verticalInput * Vector3.forward, Space.Self);
+        transform.Rotate(horizontalInput * rotationSpeed * Time.deltaTime * Vector3.up, Space.Self);
 
-        if(Input.GetKeyDown(KeyCode.Space)) {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
             playerRb.AddForce(Vector3.up * jumpStrength, ForceMode.Impulse);
             isGrounded = false;
-        }
+        }        
     }
     
     private void FixedUpdate() {
